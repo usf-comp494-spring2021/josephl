66983ce14bc569656e0d7db8b7f6f98ebaac9c82
Joseph LePage <joeschool31390@gmail.com>
2021-01-25 21:56:06 -0600

    Added hello world.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ Joseph LePage
+ 1/25/21
+ C# and .net programming
+ Hello World
+ */
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
