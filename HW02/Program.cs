2edbd5d9c62d739359508221edf56e03a43753cc
Joseph LePage <joeschool31390@gmail.com>
2021-02-08 19:47:13 -0600

    homework 2

---
+++ HW02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < 10; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    string eNum = numbers[i].ToString();
+                    System.Console.WriteLine($"{eNum} is even");
+                }
+                else
+                {
+                    string oNum = numbers[i].ToString();
+                    System.Console.WriteLine($"{oNum} is odd");
+                }//end if/else statement
+            }//end for loop
+        }
+    }
+}
